1838af79d2be12c3d6194f3017f24d798097017c
Alex <alexkumbar@gmail.com>
2014-03-19 14:32:42 -0400

    database testing and gitignore

--- public/createaccount.php
+++ public/createaccount.php
@@ -1,1 +1,14 @@
-yo
+<?php require_once "./db/init.php"; ?>
+
+<?php 
+    $query = $db->prepare(
+      "SELECT * FROM `users`"
+      );
+    $query->execute();
+    while ($user = $query->fetch()) {
+      // print_r($artist);
+      ?>
+      <h4><?php echo $user->email ?></h4>
+      <?php
+    }
+    ?>
