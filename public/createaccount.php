fda8f31ba6aa19979ff7b7085c87138158b98441
Alex <alexkumbar@gmail.com>
2014-03-19 15:55:28 -0400

    account creation

--- public/createaccount.php
+++ public/createaccount.php
@@ -10,8 +10,20 @@
       //check if fields are filled
       if (!isset($_POST['email']) || !isset($_POST['pass']) || !isset($_POST['passconfirm']) || empty($_POST['email']) || empty($_POST['pass']) || empty($_POST['passconfirm'])) {
         $msg = "Please fill in all form fields.";
-      }
+      }else if($_POST['pass'] !== $_POST['passconfirm'] ){
+        $msg = "Passwords must match";
+      }else{
+        // Generate random salt
+        $salt = hash('sha256', uniqid(mt_rand(), true));      
 
+        // Apply salt before hashing
+        $salted = hash('sha256', $salt . $_POST['pass']);
+        
+        // Store the salt with the password, so we can apply it again and check the result
+        $stmt = $db->prepare("INSERT INTO users (email, password, salt) VALUES (:email, :pass, :salt)");
+        $stmt->execute(array(':email' => $_POST['email'], ':pass' => $salted, ':salt' => $salt));
+        $msg = "Account created.";
+      }
     }else{
       $msg = "That email is already in use";
     }
@@ -24,6 +36,7 @@
   <title>make your acc</title>
 </head>
 <body>
+  <?php if (isset($msg)) echo "<p>$msg</p>" ?>
   <form method="post" action="createaccount.php">
     <label for="email">Email: </label><input type="text" name="email" />
     <label for="pass">Password: </label><input type="password" name="pass" />
