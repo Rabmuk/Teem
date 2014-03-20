78d7e6dee96618fdd4375832855bce47731ea909
Candice Poon <poonc2@rpi.edu>
2014-03-20 17:00:30 -0400

    Update header and footer

---
+++ public/bottomNav.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bottom Bar</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+	
+
+
+  <footer class="row">
+    <div class="large-12 columns">
+      <hr />
+      <div class="row">
+        
+        <div class="large-6 columns">
+          <ul class="inline-list">
+             <li><a href="index.php">Home</a></li>
+             <li><a href="faq.php">FAQ</a></li>
+             <li><a href="about.php">About Us</a></li>
+             <li><a href="contact.php">Report a Bug</a></li>
+             
+          </ul> 
+        </div>
+
+        <div class="large-6 columns">
+          <p>&copy; Team SnapChatRoulette</p>
+        </div>
+
+
+
+      </div>
+    </div> 
+  </footer>
+
+  </body>
+</html>
