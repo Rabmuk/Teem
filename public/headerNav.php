f24294a5be7edb7c645cf0a5159b75425ed405c9
Candice Poon <poonc2@rpi.edu>
2014-03-26 14:00:04 -0400

    Get rid of the end tags for bottom and top bars.

--- public/headerNav.php
+++ public/headerNav.php
@@ -1,16 +1,7 @@
 <?php
 ?>
 
-<!doctype html>
-<html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Header Bar</title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
-  </head>
-  <body>
+
 
    <div class="row">
     <div class="large-3 columns">
@@ -25,4 +16,4 @@
     </div>
   </div>
   
- </body>
+ 
