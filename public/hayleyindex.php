889d37377c50f45b951bddbe1b35e7bbeb9265e8
Hydriad <schluh@rpi.edu>
2014-03-26 09:39:53 -0400

    testing columns

--- public/hayleyindex.php
+++ public/hayleyindex.php
@@ -4,6 +4,31 @@
 require_once "./headerNav.php"; ?>
   
 <div class="row">
+  <div class="columns twelve">
+    <h2>Twelve Columns</h2>
+    <p>Lorem ipsum...</p>
+  </div>
+</div>
+ 
+<div class="row">
+    <div class="twelve columns">
+        <img src="http://placehold.it/1000x400&text=img" alt="">
+    </div>
+</div>
+ 
+<div class="row">
+  <div class="eight columns">
+    <h2>Eight Columns</h2>
+    <p>Lorem ipsum...</p>
+  </div>
+ 
+  <div class="four columns">
+    <h2>Four Columns</h2>
+    <p>Lorem ipsum...</p>
+  </div>
+</div>
+ 
+<div class="row">
   <div class="three columns">
     <h3>Three Columns</h3>
     <p>Lorem ipsum...</p>
@@ -21,6 +46,18 @@ require_once "./headerNav.php"; ?>
  
   <div class="three columns">
     <h3>Three Columns</h3>
+    <p>Lorem ipsum...</p>
+  </div>
+</div>
+ 
+<div class="row">
+  <div class="eight columns">
+    <h2>Eight Columns</h2>
+    <p>Lorem ipsum...</p>
+  </div>
+ 
+  <div class="four columns">
+    <h2>Four Columns</h2>
     <p>Lorem ipsum...</p>
   </div>
 </div>
