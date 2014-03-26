0392dc1508d0d6ceb2af7aa3f1d556e2e3ba8fdd
lcangelini <angell2@rpi.edu>
2014-03-26 14:12:52 -0400

    home/profile pages

--- public/profile.php
+++ public/profile.php
@@ -1,1 +1,31 @@
-yo
+<?php 
+	
+	require_once "./headerNav.php";
+
+?>
+<!doctype HTML>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8" />
+    <title>My Profile - Home</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+</head>
+<body>
+	<div class="fullwidth">
+		<div class="row">
+			<div class="medium-9 columns">
+				<h1 class="myProfile">Welcome</h1>
+			</div>
+			<div class="medium-3 columns">
+				Welcome
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+<?php
+
+	require_once "./bottomNav.php";
+
+?>
+
