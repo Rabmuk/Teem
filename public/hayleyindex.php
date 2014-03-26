72a038ebbd63bca0879f3519bdeaf72e97102259
Hydriad <schluh@rpi.edu>
2014-03-26 10:05:44 -0400

    totally better index with premade head/foot

--- public/hayleyindex.php
+++ public/hayleyindex.php
@@ -44,11 +44,9 @@
  
     <!-- Desktop Slider -->
  
-      <div class="hide-for-small">
+      <div class="hide-for-small" style="height:400px;width:100%;border:2px solid red;">
         <div id="featured">
-              <img src="http://placehold.it/1000x400&text=Video background" alt="slide image">
-              <!-- <img src="http://placehold.it/1000x400&text=Slide Image" alt="slide image">
-              <img src="http://placehold.it/1000x400&text=Slide Image" alt="slide image"> -->
+              <p>yoyoyoyo</p>
           </div>
         </div>
  
@@ -57,7 +55,7 @@
  
     <!-- Mobile Header -->
  
- 
+    <br>
     <div class="row">
       <div class="small-12 show-for-small"><br>
         <img src="http://placehold.it/1000x600&text=For Small Screens" />
@@ -78,20 +76,20 @@
  
         <div class="large-3 small-6 columns">
           <!--empty-->
-          <p>yo</p>
+          <p></p>
         </div>
  
         <div class="large-3 small-6 columns">
-          <a href="#" class="button">Sign Up</a>
+          <a href="#" class="button expand">Sign Up</a>
         </div>
  
         <div class="large-3 small-6 columns">
-          <a href="#" class="button">Log In</a>
+          <a href="#" class="button expand">Log In</a>
         </div>
  
         <div class="large-3 small-6 columns">
           <!--empty-->
-          <p>yo</p>
+          <p></p>
         </div>
  
     <!-- End Thumbnails -->
@@ -108,46 +106,8 @@
  
     <!-- Content -->
  
-        <div class="large-8 columns">
-          <div class="panel radius">
- 
-          <div class="row">
-          <div class="large-6 small-6 columns">
- 
-            <h4>Header</h4><hr/>
-            <h5 class="subheader">Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum odio ornare sagittis.
-            </h5>
- 
-          <div class="show-for-small" align="center">
-            <a href="#" class="small radius button">Call To Action!</a><br>
- 
-            <a href="#" class="small radius button">Call To Action!</a>
-          </div>
- 
-          </div>
-          <div class="large-6 small-6 columns">
- 
-            <p>Suspendisse ultrices ornare tempor. Aenean eget ultricies libero. Phasellus non ipsum eros. Vivamus at dignissim massa. Aenean dolor libero, blandit quis interdum et, malesuada nec ligula. Nullam erat erat, eleifend sed pulvinar ac. Suspendisse ultrices ornare tempor. Aenean eget ultricies libero.
-          </p>
-        </div>
- 
-        </div>
-        </div>
-        </div>
- 
-          <div class="large-4 columns hide-for-small">
- 
-          <h4>Get In Touch!</h4><hr/>
- 
-          <a class="large button expand" href="#">
-            Call To Action!
-          </a>
- 
-          <a class="large button expand" href="#">
-            Call To Action!
-          </a>
- 
- 
+        <div class="large-12 columns">
+          <center><p><br>New to Teem? Learn more <a href=""taget="_blank" >about us</a>.</p></center>
         </div>
  
     <!-- End Content -->
