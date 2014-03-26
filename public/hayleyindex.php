70a5e891d23b72d686e23b8b896ad634eb92a07b
Hydriad <schluh@rpi.edu>
2014-03-26 09:44:50 -0400

    test

--- public/hayleyindex.php
+++ public/hayleyindex.php
@@ -3,71 +3,69 @@
 <html class="no-js" lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>YO</title>
     <link rel="stylesheet" href="css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
 
-<div class="row">
-  <div class="columns twelve">
-    <h2>Twelve Columns</h2>
-    <p>Lorem ipsum...</p>
-  </div>
-</div>
- 
-<div class="row">
-    <div class="twelve columns">
-        <img src="http://placehold.it/1000x400&text=img" alt="">
+    <div class="row">
+      <div class="columns twelve">
+        <h2>Twelve Columns</h2>
+        <p>Lorem ipsum...</p>
+      </div>
     </div>
-</div>
- 
-<div class="row">
-  <div class="eight columns">
-    <h2>Eight Columns</h2>
-    <p>Lorem ipsum...</p>
-  </div>
- 
-  <div class="four columns">
-    <h2>Four Columns</h2>
-    <p>Lorem ipsum...</p>
-  </div>
-</div>
- 
-<div class="row">
-  <div class="three columns">
-    <h3>Three Columns</h3>
-    <p>Lorem ipsum...</p>
-  </div>
- 
-  <div class="three columns">
-    <h3>Three Columns</h3>
-    <p>Lorem ipsum...</p>
-  </div>
      
-  <div class="three columns">
-    <h3>Three Columns</h3>
-    <p>Lorem ipsum...</p>
-  </div>
- 
-  <div class="three columns">
-    <h3>Three Columns</h3>
-    <p>Lorem ipsum...</p>
-  </div>
-</div>
- 
-<div class="row">
-  <div class="eight columns">
-    <h2>Eight Columns</h2>
-    <p>Lorem ipsum...</p>
-  </div>
- 
-  <div class="four columns">
-    <h2>Four Columns</h2>
-    <p>Lorem ipsum...</p>
-  </div>
-</div>
+    <div class="row">
+        <div class="twelve columns">
+            <img src="http://placehold.it/1000x400&text=img" alt="">
+        </div>
+    </div>
+     
+    <div class="row">
+      <div class="eight columns">
+        <h2>Eight Columns</h2>
+        <p>Lorem ipsum...</p>
+      </div>
+     
+      <div class="four columns">
+        <h2>Four Columns</h2>
+        <p>Lorem ipsum...</p>
+      </div>
+    </div>
+     
+    <div class="row">
+      <div class="three columns">
+        <h3>Three Columns</h3>
+        <p>Lorem ipsum...</p>
+      </div>
+     
+      <div class="three columns">
+        <h3>Three Columns</h3>
+        <p>Lorem ipsum...</p>
+      </div>
+         
+      <div class="three columns">
+        <h3>Three Columns</h3>
+        <p>Lorem ipsum...</p>
+      </div>
+     
+      <div class="three columns">
+        <h3>Three Columns</h3>
+        <p>Lorem ipsum...</p>
+      </div>
+    </div>
+     
+    <div class="row">
+      <div class="eight columns">
+        <h2>Eight Columns</h2>
+        <p>Lorem ipsum...</p>
+      </div>
+     
+      <div class="four columns">
+        <h2>Four Columns</h2>
+        <p>Lorem ipsum...</p>
+      </div>
+    </div>
 
  </body>
     
