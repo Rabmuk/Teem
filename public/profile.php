31d3435d9a5047e3286d46573b926a4d9cb0d36b
lcangelini <angell2@rpi.edu>
2014-03-26 14:44:37 -0400

    updates

--- public/profile.php
+++ public/profile.php
@@ -25,8 +25,46 @@
 		</div>
 		<div class="row">
 			<div class="large-6 columns">
-				<div id="myProfile">
-					<p>test</p>
+				<div class="boxcolumn">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+						Nulla eu odio fringilla, gravida turpis et, rutrum purus. 
+						Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+						Integer congue mauris eget lectus vestibulum malesuada. 
+						Morbi varius quam nec ante lobortis dictum. Sed a risus quis 
+						nisl vulputate placerat. Donec eros mauris, ullamcorper ut erat
+						 quis, elementum tempus massa. Ut tincidunt a ipsum id hendrerit. 
+						 In ultricies eget risus ac pellentesque. Quisque gravida ipsum 
+						 vel elit dictum, ac elementum mauris fermentum. Nullam blandit 
+						 faucibus suscipit.
+
+						 Proin semper turpis feugiat nunc auctor, non placerat augue 
+						 commodo. Praesent tristique eros sed eros commodo condimentum. 
+						 Morbi urna quam, congue id malesuada nec, tincidunt sed ligula. 
+						 Suspendisse magna risus, pellentesque sit amet porta et, placerat 
+						 ut enim. Integer lacinia adipiscing tristique. Duis rhoncus vel 
+						 nibh vel ultrices. Donec quis arcu libero. Nam lobortis tincidunt 
+						 faucibus. Pellentesque eu commodo lorem, id venenatis arcu. Nam 
+						 rhoncus risus eu lorem feugiat, ut ultrices urna gravida. Ut 
+						 vestibulum quis risus ac hendrerit. Morbi vehicula velit ac gravida 
+						 dignissim. Nullam auctor purus nunc, sed pulvinar eros scelerisque at. 
+						 Proin vel ligula quis tellus malesuada ullamcorper. Etiam vestibulum 
+						 velit cursus posuere molestie.
+
+						 Sed purus ligula, faucibus vel est non, ornare commodo turpis. Sed 
+						 faucibus lectus est. Vivamus malesuada odio ac convallis euismod. Ut 
+						 turpis arcu, mattis vel volutpat a, adipiscing quis augue. Donec vel 
+						 augue sapien. Phasellus sagittis sapien id risus elementum, ac gravida 
+						 nisi imperdiet. Vestibulum et sapien ultrices, tempus libero malesuada, 
+						 feugiat mi.
+
+						 Nullam eu massa vehicula, tincidunt lorem vel, laoreet tortor. Mauris 
+						 sit amet tempor lorem. Proin feugiat congue mollis. In non turpis volutpat, 
+						 blandit est et, laoreet est. Vestibulum commodo lacinia sapien, vel feugiat 
+						 mauris cursus et. Vestibulum dolor risus, commodo sed magna nec, vulputate 
+						 tincidunt leo. Sed tincidunt lacinia neque. Proin egestas posuere orci. 
+						 Donec varius mollis tellus aliquet aliquet. Pellentesque nec eleifend ligula. 
+						 Nunc id justo sed sapien feugiat bibendum sed et odio. Pellentesque sagittis 
+						 orci et nisi porta dictum. Aliquam erat volutpat.</p>
 				</div>
 			</div>
 			<div class="large-6 columns">
