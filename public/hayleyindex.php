f678a10ca2180be54aff4919a88096d9543ba2d4
Hydriad <Hydriad@users.noreply.github.com>
2014-03-26 09:30:28 -0400

    Create hayleyindex.php
    texting out a new index page like in wireframes

---
+++ public/hayleyindex.php
@@ -0,0 +1,34 @@
+
+<?php
+
+require_once "./headerNav.php"; ?>
+  
+<div class="row">
+  <div class="three columns">
+    <h3>Three Columns</h3>
+    <p>Lorem ipsum...</p>
+  </div>
+ 
+  <div class="three columns">
+    <h3>Three Columns</h3>
+    <p>Lorem ipsum...</p>
+  </div>
+     
+  <div class="three columns">
+    <h3>Three Columns</h3>
+    <p>Lorem ipsum...</p>
+  </div>
+ 
+  <div class="three columns">
+    <h3>Three Columns</h3>
+    <p>Lorem ipsum...</p>
+  </div>
+</div>
+
+ <?php
+
+require_once "./bottomNav.php"; ?>
+    
+  
+
+
