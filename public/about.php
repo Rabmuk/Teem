fbfdeb4c2c6eb1382cbb8ef6384b92e86d67a818
Candice Poon <poonc2@rpi.edu>
2014-04-03 15:24:16 -0400

    Started work on the about page

--- public/about.php
+++ public/about.php
@@ -20,6 +20,8 @@
         
         <div class="row">
           
+
+          
           <div>
              <h4>Teem was created to help groups of people facilitate better and more efficient meetings. It was built with love by Team SnapChatRoulette 
              at Rensselaer Polytechnic Institutes's Web Science course taught by Richard Plotka. </h4> 
