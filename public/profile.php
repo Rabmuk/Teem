6a599f85bf053ed8a0f49d977fbf25cea1e8e654
lcangelini <angell2@rpi.edu>
2014-04-19 13:26:52 -0400

    i think i made a wrapper...

--- public/profile.php
+++ public/profile.php
@@ -20,77 +20,69 @@ require_once "./headerNav.php";
 	<link rel="stylesheet" href="css/profile.css" />
 </head>
 <body>
-<div id = "wrapper">
-	<div>
-		<div class="row">
-			<div class="large-6 columns">
-				<h1 class="myProfile">Welcome, <?php echo $user->getFirstName(); ?>!</h1>
-			</div>
-			<div class="large-3 columns">
-				<a href="creategroup.php">(+) Create a Group</a>
-			</div>
-			<div class="large-3 columns">
-				<a href="createmeeting.php">(+) Create a Meeting</a>
-			</div>
-		</div>
-		<div class="row">
-			<div class="large-6 columns">
-				<div class="boxcolumn">
-					<h2 class="profileHeader">My Groups</h2>
-					<?php 
-					$results = $user->getGroups();
-					foreach ($results as $group) {
-						?>
-						<h3 class="teamTitle"> <?php echo $group->getName(); ?> </h3>
-						<ul id="groupNames">
-							<li>Team Leader: <?php echo $group->getOwner()->getName(); ?></li>
-							<li>Members: 
-								<?php 
-									echo $group->getMemberNames();
-								?>
-							</li>
-							</ul>
-							<hr class="profileDivide"></hr>
-							<?php
-						}
-						?>
-						<!-- Insert groups here! -->
-
-					<!-- <h3 class="teamTitle">SnapchatRoullette</h3>
-					<ul id="groupNames">
-						<li>Team Leader: Not Chris</li>
-						<li>Members: Lauren, Hayley, Alex, Candice, Josh
-					</ul>
-					<hr class="profileDivide"></hr> -->
+	<div id="wrapper">
+			<div class="row">
+				<div class="large-6 columns">
+					<h1 class="myProfile">Welcome, <?php echo $user->getFirstName(); ?>!</h1>
+				</div>
+				<div class="large-3 columns">
+					<a href="creategroup.php">(+) Create a Group</a>
+				</div>
+				<div class="large-3 columns">
+					<a href="createmeeting.php">(+) Create a Meeting</a>
 				</div>
 			</div>
-			<div class="large-6 columns">
-				<div class="boxcolumn">
-					<h2 class="profileHeader">Upcoming Meetings</h2>
-					<h3 class="teamTitle">Wednesay, March 26th</h3>
-					<div class="row">
-						<div class="large-2 columns">
-							<img src="img/clock.png"></img>
+			<div class="row">
+				<div class="large-6 columns">
+					<div class="boxcolumn">
+						<h2 class="profileHeader">My Groups</h2>
+						<?php 
+						$results = $user->getGroups();
+						foreach ($results as $group) {
+							?>
+							<h3 class="teamTitle"> <?php echo $group->getName(); ?> </h3>
+							<ul id="groupNames">
+								<li>Team Leader: <?php echo $group->getOwner()->getName(); ?></li>
+								<li>Members: 
+									<?php 
+										echo $group->getMemberNames();
+									?>
+								</li>
+								</ul>
+								<hr class="profileDivide"></hr>
+								<?php
+							}
+							?>
+							<!-- Insert groups here! -->
+
+						<!-- <h3 class="teamTitle">SnapchatRoullette</h3>
+						<ul id="groupNames">
+							<li>Team Leader: Not Chris</li>
+							<li>Members: Lauren, Hayley, Alex, Candice, Josh
+						</ul>
+						<hr class="profileDivide"></hr> -->
+					</div>
+				</div>
+				<div class="large-6 columns">
+					<div class="boxcolumn">
+						<h2 class="profileHeader">Upcoming Meetings</h2>
+						<h3 class="teamTitle">Wednesay, March 26th</h3>
+						<div class="row">
+							<div class="large-2 columns">
+								<img src="img/clock.png"></img>
+							</div>
+							<div class="large-10 columns">
+								<h4>3:00 PM - SnapchatRoulette</h4>
+							</div>
+							<hr>
 						</div>
-						<div class="large-10 columns">
-							<h4>3:00 PM - SnapchatRoulette</h4>
-						</div>
-						<hr>
 					</div>
 				</div>
 			</div>
 		</div>
-<<<<<<< HEAD
-	</div>
-</div>
-</body>
-</html>
-<?php
-=======
 	</body>
 	</html>
 	<?php
->>>>>>> 1a1da9670dfb0e701f332299d93a1d80f8eeeb11
 
 	require_once "./bottomNav.php";
 
