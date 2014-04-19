3f6860a859f020cfc61df371e29df4e89d86e3fc
Candice Poon <poonc2@rpi.edu>
2014-04-19 12:26:18 -0400

    Rebuilt the footer
    Matched the spacing of the header. Too bad Lauren broke everything.

--- public/bottomNav.php
+++ public/bottomNav.php
@@ -3,19 +3,18 @@
 <?php
 ?>
 
-  <footer class="footer" id = "footer">
-          <ul class="inline-list">
-             <li><a href="index.php">Home</a></li>
-             <li><a href="about.php">About</a></li>
-             <li><a href="contact.php">Contact Us</a></li>
-          
-        <div class="inline right">
-          <p>&copy; Team SnapChatRoulette</p>
-        </div>
-          </ul> 
+ <footer class="row header">
+    
+    <div class="large-9 columns">
+      <ul class="inline-list left">
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+      </ul>
+    </div>
 
-
-
+    <div class="large-3 columns">
+        <p>&copy; Team SnapChatRoulette</p>
+    </div>
   </footer>
 
   </body>
