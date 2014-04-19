3d199b01dda43e3e01356d7482d0cd095ad25b40
Hydriad <schluh@rpi.edu>
2014-04-19 10:20:22 -0400

    creatagenda shell done!

--- public/createagenda.php
+++ public/createagenda.php
@@ -49,17 +49,24 @@
 					<p>1)</p>
 				</div>
 				<div class="large-3 columns">
-					<input type="text" name="topic" value="topic">
+					<input type="text" name="topic" value="topic" onFocus="if(this.value=='topic')this.value='';">
 				</div>
 				<div class="large-3 columns">
-					<input type="text" name="people" value="people">
+					<input type="text" name="people" value="people" onFocus="if(this.value=='people')this.value='';">
+				</div>
+				<div class="large-1 columns">
+					<input type="text" name="duration" value="#" onFocus="if(this.value=='#')this.value='';">
 				</div>
 				<div class="large-2 columns">
-					<input type="text" name="duration" value="duration">
+					<select>
+					  <option>Minutes</option>
+					  <option>Seconds</option>
+					  <option>Hours</option>
+					</select>
 				</div>
-				<div class="large-3 columns">
+				<div class="large-2 columns">
 					<p> </p>
-				</div>
+				</div>	
 			</div>
 		</form>
 		<!--form2-->
@@ -69,17 +76,24 @@
 					<p>2)</p>
 				</div>
 				<div class="large-3 columns">
-					<input type="text" name="topic" value="topic">
+					<input type="text" name="topic" value="topic" onFocus="if(this.value=='topic')this.value='';">
 				</div>
 				<div class="large-3 columns">
-					<input type="text" name="people" value="people">
+					<input type="text" name="people" value="people" onFocus="if(this.value=='people')this.value='';">
+				</div>
+				<div class="large-1 columns">
+					<input type="text" name="duration" value="#" onFocus="if(this.value=='#')this.value='';">
 				</div>
 				<div class="large-2 columns">
-					<input type="text" name="duration" value="duration">
+					<select>
+					  <option>Minutes</option>
+					  <option>Seconds</option>
+					  <option>Hours</option>
+					</select>
 				</div>
-				<div class="large-3 columns">
+				<div class="large-2 columns">
 					<p> </p>
-				</div>
+				</div>	
 			</div>
 		</form>
 		<!--form3-->
@@ -89,28 +103,54 @@
 					<p>3)</p>
 				</div>
 				<div class="large-3 columns">
-					<input type="text" name="topic" value="topic">
+					<input type="text" name="topic" value="topic" onFocus="if(this.value=='topic')this.value='';">
 				</div>
 				<div class="large-3 columns">
-					<input type="text" name="people" value="people">
+					<input type="text" name="people" value="people" onFocus="if(this.value=='people')this.value='';">
+				</div>
+				<div class="large-1 columns">
+					<input type="text" name="duration" value="#" onFocus="if(this.value=='#')this.value='';">
 				</div>
 				<div class="large-2 columns">
-					<input type="text" name="duration" value="duration">
+					<select>
+					  <option>Minutes</option>
+					  <option>Seconds</option>
+					  <option>Hours</option>
+					</select>
 				</div>
-				<div class="large-3 columns">
+				<div class="large-2 columns">
 					<p> </p>
-				</div>
+				</div>	
 			</div>
 		</form>
 		<!--add more-->
 		<div class="row">
 			<div class="large-1 columns"><p></p></div>
 			<div class="large-11 columns">
-				<a href="">(+) Add more</a>
-				<br><br><br><br><br><br><br><br><br><br><br><br>
+				<a href="">(+) Add more</a><br><br>
+			</div>
+		</div>
+		<!--Submit button-->
+		<div class="row">
+			<div class="large-2 columns">
+				<br><button type="button">Done!</button>
+			</div>
+			<div class="large-10 columns">
+				<p> </p>
+			</div>
+		</div>
+		<!--disclaimer-->
+		<div class="row">
+			<div class="large-7 columns">
+				<p>*People can attach their own files and action items to customize their portions of the meeting once the agenda is created.</p>
+			</div>
+			<div class="large-5 columns">
+				<p></p>
 			</div>
 		</div>
 
+<!--don't worry about this-->
+<br><br><br><br><br><br><br><br><br><br><br><br>
 </body>
 </html>
 <?php
