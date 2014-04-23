88185590a099b740b7886cc99fcc95eae8433c7a
Hydriad <schluh@rpi.edu>
2014-04-23 13:23:23 -0400

    agenda

--- public/agenda.php
+++ public/agenda.php
@@ -13,11 +13,12 @@
 <body>
 <div id = "wrapper">
 	<div class="row">
-		<div class="large-12 columns colorback">
+		<div class="large-12 columns">
 			<h1 class= "meetingName">Web Science Meeting Agenda</h1>
 			<p class= "desiredOutcome">Desired outcome: TO BEAT THE OTHER SCHEDULING TEAM >:D</p>
 		</div>
 	</div>
+	<!--begin one person's section-->
 	<!--headings-->
 	<div class="row">
 		<div class="large-12 columns">
@@ -39,6 +40,33 @@
 			<input type="submit" name="savetopics" value="Save" />
 		</div>
 	</div>
+	<center><hr style="width:80%;"></center>
+	<!--end one person's section-->
+	<!--begin one person's section-->
+	<!--headings-->
+	<div class="row">
+		<div class="large-12 columns">
+			<br><h3 class = "heading">Colors and stuff</h3>
+			<p class = "inline right">15 minutes</p>
+			<p>Hayley Schluter</p>
+		</div>
+	</div>
+	<!--topics, attachments-->
+	<div class="row">
+		<div class="large-6 columns">
+			<h5 class= "heading">Topics to Cover</h5>
+			<textarea rows="4" onFocus="if(this.value=='Enter topics here.')this.value='';">Enter topics here.</textarea>
+			<input type="submit" name="savetopics" value="Save" />
+		</div>
+		<div class="large-6 columns">
+			<h5 class= "heading">Attachments</h5>
+			<input type="submit" name="uploadfile" value="Upload File" />
+			<input type="submit" name="savetopics" value="Save" />
+		</div>
+	</div>
+	<center><hr style="width:80%;"></center>
+	<!--end one person's section-->
+
 </div>
 
 <!--don't worry about this-->
