3b1c20a8650a85aa1f7310335b096320c4d7ea8c
lcangelini <angell2@rpi.edu>
2014-04-27 16:29:22 -0400

    edit group

--- public/editgroup.php
+++ public/editgroup.php
@@ -15,37 +15,73 @@ require_once "./headerNav.php";
 ?>
 <!doctype HTML>
 <html>
+<head>
+  <title>Edit Group</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="css/editgroup.css"/>
+</head>
  <body>
-  <h1> Edit Group </h1>
-  <div>
- <?php 
-    $results = $user->getGroups();
-    $arraycounter  = 0;
-    foreach ($results as $group) {
-     ?>
-     <h3"teamTitle"> <?php echo $group->getName(); ?></h3>
-     <ul id="groupNames">
-      <li>Team Leader: <?php echo $group->getOwner()->getName(); ?></li>
-      <li>Members: 
-       <?php 
-       echo $group->getMemberNames();
-       ?>
-       </li>
-      </ul>
-     <?php 
-      }
-       ?>
-    <div id="wrapper">
-      <form action="" method="post">
-        <select name="member" id="member" class="update">
-          <option value=""> Members</option>
-        </select>
+  <div id="wrapper">
+    <div class="row">
+      <div class="large-12 columns">
+        <h1> Edit Group </h1>
+      </div>
     </div>
-  </div> 
+    <?php 
+      $results = $user->getGroups();
+      $arraycounter  = 0;
+      foreach ($results as $group) {
+    ?>
+    <form action="" method="post">
+      <div class="row">
+        <div class="small-3 columns">
+          <label for="changeName" class="right inline">Change Group Name: </label>
+        </div>
+        <div class="small-9 columns">
+          <input type="text" id="changeName" name="changeName" value="<?php echo $group->getName(); ?>">
+        </div>
+      </div>
+      <?php } ?>
+      <div class="row">
+        <div class="small-3 columns">
+          <label for="addMember" class="right inline">Add Members</label>
+        </div>
+        <div class="small-9 columns">
+          <input type="text" id="addMember" name="addMember" placeholder="Enter Member Name">
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-3 columns">
+          <label for="deleteMemembers" class="right inline">Delete Members</label>
+        </div>
+        <div class="small-9 columns">
+          <ul id="deleteMembers">
+          <?php
+          $members = $group->getMemberArray();
+          $counter = 0;
+          foreach ($members as $value) {
+            echo "<li><input id='deleteMember[$counter]' type='checkbox'><label for='deleteMember[$counter]'>$value</label></input></li>";
+            $counter++;
+          }
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-3 columns"></div>
+        <div class="small-9 columns">
+          <input id="deleteAcc" type="checkbox"><label for="deleteAcc">Delete Group?</label></input>
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-3 columns">
+          <input type="submit" name="submit" value="Save" class="button small expand"></input>
+        </div>
+      </div>
+    </form>
+  </div>
  </body>
 </html>
+
 <?php
-
 require_once "./bottomNav.php";
-
 ?>
