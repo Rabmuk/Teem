acdaef0569dd92ca0d36ff89211a4b5d9750f40a
Candice Poon <poonc2@rpi.edu>
2014-04-30 14:51:09 -0400

    Rebuilding Agenda

--- public/agenda.php
+++ public/agenda.php
@@ -75,174 +75,180 @@ require_once "./headerNav.php";
 	<script src="agenda.js"></script>
 </head>
 <body>
-	<div id = "wrapper">
+
+
+	<div class="wrapper">
+		
+		<!-- Name of meeting and desire outcome -->	
 		<div class="row">
-			<div class="large-10 columns">
+			<div class="large-9 columns" id="DesiredOutcomes">
 				<h1 class= "meetingName"><?php echo $meeting->getTitle(); ?></h1>
 				<p class= "desiredOutcome">Desired outcome: <?php echo $meeting->getDescription(); ?></p>
-
-				<!--begin one person's section-->
-				<!--headings-->
-				<?php 
-				$agendaItems = $meeting->getAgendaItems();
-				foreach($agendaItems as $agendaItem){
-					?>
-					<div class="row">
-						<div class="large-12 columns">
-							<br><h3 class = "heading"><?php echo $agendaItem->getHeading(); ?></h3>
-							<p class = "inline right"><?php echo $agendaItem->getTime(); ?> minutes</p>
-							<p><?php echo $agendaItem->getPresenter()->getName(); ?></p>
-						</div>
-					</div>
-					<!--topics, attachments-->
-					<div class="row">
-						<div class="large-6 columns">
-							<h5 class= "heading">Topics to Cover</h5>
-							<ul>
-								<?php 
-								$topics = $agendaItem->getTopics();
-								foreach ($topics as $topic) {
-									echo "<li>" . $topic . "</li>";
-								}
-								?>
-							</ul>
-							<?php 
-							$isPresenter = $user->isUser($agendaItem->getPresenter());
-							if ($isPresenter) {
-								?>
-								<form method="post">
-									<input type="text" name="topics" placeholder="New Topic">
-									<input type="hidden" name="item_id" value="<?php echo $agendaItem->getID(); ?>">
-								</form>
-								<form method="post">
-									<input type="submit" name="clearTopics" value="Clear" />
-									<input type="hidden" name="item_id" value="<?php echo $agendaItem->getID(); ?>">
-								</form>
-								<?php
-							}
-							?>
-						</div>
-						
-						<div class="large-6 columns">
-							<h5 class= "heading">Attachments</h5>
-							<ul>
-								<?php
-								$files = $agendaItem->getFiles();
-								foreach ($files as $file) {
-									?>
-									<a href="uploads/<?php echo $file->getLocation(); ?>" target="_blank"><?php echo $file->getName(); ?></a>
-
-									<?php
-								}
-								?>
-							</ul>
-							<?php
-							if ($isPresenter) {
-								?>
-
-								<form action="" method="post" enctype="multipart/form-data">
-									<input type="hidden" name="item_id" value=<?php echo '"' . $agendaItem->getID() . '"'; ?>>
-									<label for="file">Filename:</label>
-									<input type="file" name="file" id="file"><br>
-									<input type="submit" name="savefile" value="Submit">
-								</form>
-
-								<?php
-							}
-							?>
-						</div>
-
-					</div>
-
-					
-					<center><hr style="width:80%;"></center>
-					<?php 
-				}
-				?>
-
-				
-
-
-				<div id="myModal" class="reveal-modal small" data-reveal>
-					<h1>Add meeting item</h1>
-					<form method="post" action=<?php echo '"?id=' . $_GET['id'] . '"'; ?>>
-						<input type="text" name="heading" placeholder="Enter item heading">
-						<input type="number" name="time" placeholder="Enter allotted minutes">
-						<input type="email" name="presenter" placeholder="Enter presentor's email">
-						<input type="submit" name="addItem" value="Submit" class="button small expand"></input>
-						<a class="close-reveal-modal">&#215;</a>
-						
-					</form>
-				</div>
 			</div>
 
-			<div class="large-2 columns">
-				<!-- Action items *TODO* FOR ALEX-->
-				<!-- It's a list of each member's name, with a list of their action items. Underneath it is the ability to add tasks -->
 
-				
-				<div class="actionItems">
-					<!-- Each individual action item list -->
-					<h3>Action Items</h3>
-					<?php 
-					$members = $meeting->getMemberArray();
-					foreach ($members as $member) {
-						?>
-						<div class="row">
-							<!-- meeting attendee's name -->
-							<h4 class="memberName"><?php echo $member->getName(); ?></h4>
-							<!-- List of existing tasks -->
-							<ul class="tasks">
-								<?php
-								foreach ($member->getActionItems($meeting->getID()) as $action) {
-									echo '<li>' . $action->getAction() . '</li>';
-								}
-								?>
-							</ul>
-							<!-- Add a new task -->
-							<!-- Hitting enter will input the form. Javascript located in agenda.js -->
-							<form method="post">
-								<input type="text" name="newtask" placeholder="New Task">
-								<input type="hidden" name="id_user" value="<?php echo $member->getID(); ?>">
-							</form>
-							<form method="post">
-								<input type="submit" name="clearActions" value="Clear" />
-								<input type="hidden" name="id_user" value="<?php echo $member->getID(); ?>">
-							</form>
-
-
-						</div>
-
-						<?php
-					}
-					?>
-
-				</form>
-
-			</div>
 		</div>
 
+		<!-- The actual agenda material, including topics and action items -->
+		<div class="row agendaRow">
+			
+				<div class="large-9 columns" id="meetingTasks">
+					<?php 
+						$agendaItems = $meeting->getAgendaItems();
+						foreach($agendaItems as $agendaItem){
+							?>
+							<div class="row">
+								<div class="large-12 columns">
+									<br><h3 class = "heading"><?php echo $agendaItem->getHeading(); ?></h3>
+									<p class = "inline right"><?php echo $agendaItem->getTime(); ?> minutes</p>
+									<p><?php echo $agendaItem->getPresenter()->getName(); ?></p>
+								</div>
+							</div>
+							<!--topics, attachments-->
+							<div class="row">
+								<div class="large-6 columns">
+									<h5 class= "heading">Topics to Cover</h5>
+									<ul>
+										<?php 
+										$topics = $agendaItem->getTopics();
+										foreach ($topics as $topic) {
+											echo "<li>" . $topic . "</li>";
+										}
+										?>
+									</ul>
+									<?php 
+									$isPresenter = $user->isUser($agendaItem->getPresenter());
+									if ($isPresenter) {
+										?>
+										<form method="post">
+											<input type="text" name="topics" placeholder="New Topic">
+											<input type="hidden" name="item_id" value="<?php echo $agendaItem->getID(); ?>">
+										</form>
+										<form method="post">
+											<input type="submit" name="clearTopics" value="Clear" />
+											<input type="hidden" name="item_id" value="<?php echo $agendaItem->getID(); ?>">
+										</form>
+										<?php
+									}
+									?>
+								</div>
+								
+								<div class="large-6 columns">
+									<h5 class= "heading">Attachments</h5>
+									<ul>
+										<?php
+										$files = $agendaItem->getFiles();
+										foreach ($files as $file) {
+											?>
+											<a href="uploads/<?php echo $file->getLocation(); ?>" target="_blank"><?php echo $file->getName(); ?></a>
+
+											<?php
+										}
+										?>
+									</ul>
+									<?php
+									if ($isPresenter) {
+										?>
+
+										<form action="" method="post" enctype="multipart/form-data">
+											<input type="hidden" name="item_id" value=<?php echo '"' . $agendaItem->getID() . '"'; ?>>
+											<label for="file">Filename:</label>
+											<input type="file" name="file" id="file"><br>
+											<input type="submit" name="savefile" value="Submit">
+										</form>
+
+										<?php
+									}
+									?>
+								</div>
+
+							</div>
+
+							
+							<!-- <center><hr style="width:80%;"></center> -->
+							<?php 
+						}
+						?>
+
+						<div id="myModal" class="reveal-modal small" data-reveal>
+							<h1>Add meeting item</h1>
+							<form method="post" action=<?php echo '"?id=' . $_GET['id'] . '"'; ?>>
+								<input type="text" name="heading" placeholder="Enter item heading">
+								<input type="number" name="time" placeholder="Enter allotted minutes">
+								<input type="email" name="presenter" placeholder="Enter presentor's email">
+								<input type="submit" name="addItem" value="Submit" class="button small expand"></input>
+								<a class="close-reveal-modal">&#215;</a>
+								
+							</form>
+						</div>
+
+						<div class="row">
+							<a href="#" data-reveal-id="myModal" class="button exapand" data-reveal>Add meeting item</a>
+						</div>
+
+
+
+				</div>
+
+
+
+				<div class="large-3 columns" id="agendaItems">
+					
+					<div class="actionItems">
+						<!-- Each individual action item list -->
+						<h3>Action Items</h3>
+						<?php 
+						$members = $meeting->getMemberArray();
+						foreach ($members as $member) {
+							?>
+							<div class="row">
+								<!-- meeting attendee's name -->
+								<h4 class="memberName"><?php echo $member->getName(); ?></h4>
+								<!-- List of existing tasks -->
+								<ul class="tasks">
+									<?php
+									foreach ($member->getActionItems($meeting->getID()) as $action) {
+										echo '<li>' . $action->getAction() . '</li>';
+									}
+									?>
+								</ul>
+								<!-- Add a new task -->
+								<!-- Hitting enter will input the form. Javascript located in agenda.js -->
+								<form method="post">
+									<input type="text" name="newtask" placeholder="New Task">
+									<input type="hidden" name="id_user" value="<?php echo $member->getID(); ?>">
+								</form>
+								<form method="post">
+									<input type="submit" name="clearActions" value="Clear" />
+									<input type="hidden" name="id_user" value="<?php echo $member->getID(); ?>">
+								</form>
+
+
+
+
+							</div>
+
+							<?php
+						}
+						?>
+
+					</div>
+
+				</div>
+
+			
+		</div>
+
+
+
+
 	</div>
 
 
 
-	<center><hr style="width:80%;"></center>
-
-
-	<div class="row">
-		<a href="#" data-reveal-id="myModal" class="button exapand" data-reveal>Add meeting item</a>
-	</div>
 
 
 
-
-
-</div>
-
-
-
-
-</div>
 
 
 
