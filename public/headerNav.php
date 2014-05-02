34732732c65739b1c3523a2fcde346551f1442c3
Hydriad <schluh@rpi.edu>
2014-05-01 20:51:54 -0400

    commenting headerNav done

--- public/headerNav.php
+++ public/headerNav.php
@@ -5,16 +5,19 @@
 
    <div class="row header">
     <div class="large-3 columns">
-      
+      <!--logo and links to home-->
       <a id="logo" href="index.php"><img src="img/logo-mini.png" />Teem</a>
     </div>
     
     <div class="large-9 columns">
       <ul class="inline-list right" id = "headerlist">
+        <!--links to other parts of site-->
         <li><a href="profile.php">Home</a></li>
         <li><a href="accountsettings.php">Settings</a></li>
+        <!--if logged in, displays log out option-->
         <?php if(isset($_SESSION['email'])){ ?>
           <li><a href="logout.php">Log Out</a></li>
+        <!--if logged out, displays log in option-->
         <?php }else{ ?>
           <li><a href="login.php">Log In</a></li>
         <?php } ?>
