ac8c1d6cafb2eeaade546664234559763d67061b
Chris Paradis <paradc2@rpi.edu>
2014-05-01 23:02:14 -0400

    Wait we dont need that

--- public/about.php
+++ public/about.php
@@ -2,7 +2,7 @@
 <?php include "./headerNav.php"; ?>   
 <html>
     <head>
-      <!-- Include the files needed -->
+      
         <meta charset="utf-8">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
