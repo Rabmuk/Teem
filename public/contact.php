12b92b939fc80f0f5bd5b02a6f93d75064969637
Hydriad <schluh@rpi.edu>
2014-05-01 20:40:54 -0400

    commenting contact done

--- public/contact.php
+++ public/contact.php
@@ -1,3 +1,4 @@
+<!--header, yo-->
 <?php
     require_once "headerNav.php";
 ?>
@@ -5,6 +6,7 @@
 <!doctype html>
 <html class="no-js" lang="en">
   <head>
+  	<!--all links-->
     <meta charset="utf-8" />
     <title>Contact</title>
     <link rel="stylesheet" href="css/foundation.css">
@@ -23,23 +25,25 @@
 
 				
 		        <?php if(isset($emailSent) && $emailSent == true) { ?>
-	                
+	            <!--sent email notification if you send it right-->    
 		       	<div class="large-6 columns">
 	                <p class="info">Your email was sent. Huzzah!</p>
 	            </div>
 	            <?php } else { ?>
+	            <!--title/explanation-->
 	            <h1 style="color:black;">Contact Us</h1>
 				<p class="desc">You can use this contact form to report any bugs, ask a question, or send a message to the team. All fields are required.</p>
-			
+				<!--if there's an error, don't send it yet-->
 				<?php if(isset($hasError) || isset($captchaError) ) { ?>
 	                <p id="alert">Error submitting the form.</p>
 	            <?php } ?>
 				
 				
-
+	            <!--physical form starts-->
 				<form id="contact-us" class="" action="#" method="post">
 					<div class="row">
 						<div class="large-6 columns">
+							<!--enter name-->
 							<input type="text" name="contactName" id="contactName" value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>" class="txt requiredField messageinput name" placeholder="Name" onclick="clearTextArea(this);"/>
 							<?php if($nameError != '') { ?>
 								<br /><span class="error"><?php echo $nameError;?></span> 
@@ -49,6 +53,7 @@
 	                
 					<div class="row">
 						<div class="large-6 columns">
+							<!--enter email address-->
 							<input type="text" name="email" id="email" value="<?php if(isset($_POST['email']))  echo $_POST['email'];?>" class="txt requiredField email messageinput" placeholder="Email" onclick="clearTextArea(this);" />
 							<?php if($emailError != '') { ?>
 								<br /><span class="error"><?php echo $emailError;?></span>
@@ -58,12 +63,14 @@
 	                
 					<div class="row">
 						<div class="large-6 columns">
+							<!--enter message to send-->
 							 <textarea name="comments" id="commentsText" class="txtarea requiredField messageinput messageinputtextarea message" placeholder="Message" onclick="clearTextArea(this);"><?php if(isset($_POST['comments'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['comments']); } else { echo $_POST['comments']; } } ?></textarea>
 							<?php if($commentError != '') { ?>
 								<br /><span class="error"><?php echo $commentError;?></span> 
 							<?php } ?>
 						</div>
 					</div>
+						<!--submit that jazz-->
 	                	<button class = "button" name="submit" type="submit" id="submitbutton">Send</button>
 						<input type="hidden" name="submitted" id="submitted" value="true" />
 				</form>			
@@ -75,6 +82,7 @@
 	</div>
 </div>
 </body>
+<!--footer, yo-->
 <?php
   require_once "bottomNav.php";
 
